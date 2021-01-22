bbe0530a607d668f34124e05720907bc2879bbe6
vickyramgude21 <77781557+vickyramgude21@users.noreply.github.com>
2021-01-22 12:47:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+import redis
+import os
+
+REDIS_HOST="127.0.0.1"
+REDIS_PORT=6379
+BHAV_COPY_URL="https://www.bseindia.com/markets/MarketInfo/BhavCopy.aspx"
+
+def get_redis_connection():
+    """
+    build connection to redis
+    returns:
+        dict:
+            success response : {"status":1,"data":"--success message--"}
+            fail response : {"status":0,"data":"--error message--"}
+
+    """
+    res = {"status": 0, "data": ""}
+    try:
+        # r = redis.StrictRedis(host='localhost',charset="utf-8",decode_responses=True, port=6379, db=0)
+        r = redis.from_url(os.environ.get("REDIS_URL"),charset="utf-8",decode_responses=True, db=0)
+        res["status"]=1
+        res["data"]=r
+    except Exception as e:
+        res["data"]="Redis connection error, Kindly contact administrator"
+    return res
+
